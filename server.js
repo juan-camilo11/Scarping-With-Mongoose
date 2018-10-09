5586451f58e3266324ba32219786c4677ac79dae
Juan Varon <juancamilo_11@utexas.edu>
2018-10-08 21:45:32 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const bodyParser = require("body-parser")
+const path = require("path");
+
+const db = require("./models");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//parse application/json
+app.use(bodyParser.json());
+
+//require handlebars
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLAyout: "main"}));
+app.set("view engine", "handlebars");
+
+//server static content from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+require("./routes/api-routes")(app,axios,cheerio);
+require("./routes/html-routes")(app);
+
+
+
+app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost" + PORT);
+});
